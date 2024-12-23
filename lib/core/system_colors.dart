34e8f75eec08f9d7a6ebb68552e009876bf71426
Julianafrego <julianafgomes@aluno.uespi.br>
2024-12-23 17:59:58 -0300

    feat: adding more colors

--- lib/core/system_colors.dart
+++ lib/core/system_colors.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 
 class SystemColors {
   static const Color white = Color.fromRGBO(218, 255, 253, 1);
-  static const Color blue1 = Color.fromRGBO(65, 193, 186, 1);
+  static const Color white2 = Color.fromRGBO(179, 215, 221, 1);
+  static const Color blue1 = Color.fromRGBO(46, 131, 156, 1);
   static const Color blue2 = Color.fromRGBO(54, 91, 109, 1);
   static const Color blue3 = Color.fromRGBO(35, 56, 67, 1);
-
+  static const Color red = Color.fromRGBO(198, 7, 17, 1);
+  static const Color green = Color.fromRGBO(0, 128, 0, 1);
+  static const Color green2 = Color.fromRGBO(97, 209, 116, 1);
+  static const Color black = Color.fromRGBO(0, 0, 0, 1);
 
 }
